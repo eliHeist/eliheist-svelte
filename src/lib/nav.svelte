f32bb719e7015e5d486f00351abb17ef6f57a2fd
Elijah <eliyang256@outlook.com>
2024-03-28 18:10:07 +0300

    new style

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,21 +1,26 @@
-<nav id="main-nav" class="sticky top-0 w-screen">
-    <div class="container mx-auto">
-        <div class="flex justify-center sm:justify-between py-1">
-            <a class="font-bold text-primary-500 p-1 text-2xl" href="./">eliHeist</a>
-            <div id="menu" class="hidden sm:inline fixed sm:relative bottom-0 left-0 right-0">
-                <menu class="bg-primary-500 sm:bg-transparent grid place-content-center p-8 sm:p-0">
-                    <ul class="flex flex-col sm:flex-row uppercase w-fit mb-12 sm:mb-0">
-                        <li class="py-4 px-2 sm:py-2 hover:text-primary-500"><a href="/">Home</a></li>
-                        <li class="py-4 px-2 sm:py-2 hover:text-primary-500"><a href="/projects">Projects</a></li>
-                        <li class="py-4 px-2 sm:py-2 hover:text-primary-500"><a href="/contact">Contact</a></li>
-                    </ul>
-                </menu>
-            </div>
-        </div>
-    </div>
-</nav>
+<header class="page_header z-50">
+    <nav class="main_nav">
+        <menu>
+            <a href="/" class="menu_item">
+                <span class="active">Home</span>
+            </a>
+            <a href="/about" class="menu_item">
+                <span>About</span>
+            </a>
+            <a href="/portfolio" class="menu_item">
+                <span>Portfolio</span>
+            </a>
+            <a href="/contact" class="menu_item">
+                <span>Contact</span>
+            </a>
+        </menu>
+    </nav>
+</header>
 
-<button title="Menu button" class="hamburger-btn grid grid-flow-col sm:hidden z-50" aria-expanded="false">
+<button
+    title="Menu button"
+    class="hamburger-btn grid grid-flow-col sm:hidden z-50"
+    aria-expanded="false">
     <!-- <svg class="hamburger w-10" fill="none" viewbox="-10 -10 120 120">
         <path class="line" d="m 20 40 h 60 a 1 1 0 0 1 0 20 h -60 a 1 1 0 0 1 0 -40 h 30 v 70"></path>
     </svg> -->
@@ -23,3 +28,35 @@
         <span>Menu</span>
     </div>
 </button>
+
+<style lang="postcss">
+    .page_header {
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        color: var(--dark);
+        .main_nav {
+            display: flex;
+            justify-content: center;
+
+            menu {
+                display: flex;
+                gap: 1rem;
+                padding: 1rem 0;
+
+                span {
+                    padding: 0.5rem 1rem;
+                    &.active {
+                        background-color: var(--grey-900);
+                        color: var(--white);
+                    }
+                    &:hover:not(.active) {
+                        background-color: var(--grey-600);
+                        color: var(--white);
+                    }
+                }
+            }
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,15 @@
 <script>
-
-    import Nav from "$lib/nav.svelte";
-
 </script>
 
 <main>
     <!-- banner -->
-    <section id="banner" class="h-screen w-screen grid place-content-center text-center">
-        <div class="">
-            <h2 class="font-semibold text-primary-500 text-3xl sm:text-6xl mb-8">Hi, I'm Elijah</h2>
-            <h3 class="font-bold text-5xl sm:text-7xl">I make Web Applications</h3>
+    <section id="banner" class="banner">
+        <img src="img/laptop-banner.webp" class="banner_image" alt="" />
+        <div class="banner__content">
+            <h1 class="banner__title">Hi, I'm <span>Elijah</span></h1>
+            <p>Specializing in full-stack Web Development</p>
         </div>
     </section>
-
-    <Nav/>
 
     <!-- service cards -->
     <section id="service-cards" class="py-12">
@@ -25,21 +21,30 @@
                 <article class="home-card">
                     <h3 class="title">UI Development</h3>
                     <p class="lead">
-                        Crafting captivating interfaces that blend creativity and cutting-edge technologies, I transform ideas into visually stunning user experiences that leave a lasting impact.
+                        Crafting captivating interfaces that blend creativity
+                        and cutting-edge technologies, I transform ideas into
+                        visually stunning user experiences that leave a lasting
+                        impact.
                     </p>
                     <a href="./"><i class="fas fa-eye"></i> Know More</a>
                 </article>
                 <article class="home-card highlight">
                     <h3 class="title">Backend Development</h3>
                     <p class="lead">
-                        With expertise in Django and SvelteKit, I engineer robust back-end systems that power seamless functionality, ensuring your applications are both visually impressive and technically solid.
+                        With expertise in Django and SvelteKit, I engineer
+                        robust back-end systems that power seamless
+                        functionality, ensuring your applications are both
+                        visually impressive and technically solid.
                     </p>
                     <a href="./"><i class="fas fa-eye"></i> Know More</a>
                 </article>
                 <article class="home-card">
                     <h3 class="title">Web Deployment</h3>
                     <p class="lead">
-                        From integration to performance optimization, I handle the entire deployment process, ensuring your web applications are securely and efficiently launched for a global audience.
+                        From integration to performance optimization, I handle
+                        the entire deployment process, ensuring your web
+                        applications are securely and efficiently launched for a
+                        global audience.
                     </p>
                     <a href="./"><i class="fas fa-eye"></i> Know More</a>
                 </article>
@@ -50,13 +55,13 @@
     <section id="skills" class="py-12">
         <div class="container mx-auto px-2">
             <div class="flex flex-wrap justify-center gap-8">
-                <img src="img/skills/js-logo.png" alt="" class="h-8">
-                <img src="img/skills/ts-logo.png" alt="" class="h-8">
-                <img src="img/skills/sass-logo.png" alt="" class="h-8">
-                <img src="img/skills/tailwind-css.png" alt="" class="h-8">
-                <img src="img/skills/mysql-logo.png" alt="" class="h-8">
-                <img src="img/skills/python-logo.png" alt="" class="h-8">
-                <img src="img/skills/django-logo.png" alt="" class="h-8">
+                <img src="img/skills/js-logo.png" alt="" class="h-8" />
+                <img src="img/skills/ts-logo.png" alt="" class="h-8" />
+                <img src="img/skills/sass-logo.png" alt="" class="h-8" />
+                <img src="img/skills/tailwind-css.png" alt="" class="h-8" />
+                <img src="img/skills/mysql-logo.png" alt="" class="h-8" />
+                <img src="img/skills/python-logo.png" alt="" class="h-8" />
+                <img src="img/skills/django-logo.png" alt="" class="h-8" />
             </div>
         </div>
     </section>
@@ -67,22 +72,41 @@
                 <h2>My Projects.</h2>
             </header>
             <div class="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-                
                 <article class="project-card">
                     <figure class="img-control">
-                        <img class="img-fluid" src="" alt="">
+                        <img class="img-fluid" src="" alt="" />
                     </figure>
                     <div class="info">
                         <h3>Title</h3>
-                        <a href="/"><i class="fas fa-eye"></i> Know
-                            more</a>
+                        <a href="/"><i class="fas fa-eye"></i> Know more</a>
                     </div>
                 </article>
-                    
             </div>
         </div>
     </section>
 </main>
 
 <style lang="postcss">
+    .banner {
+        max-height: 100svh;
+        display: grid;
+        align-content: end;
+        position: relative;
+
+        .banner__content {
+            position: absolute;
+            inset: 0;
+            z-index: 2;
+            display: grid;
+            place-content: center;
+
+            h1 {
+                text-align: center;
+                font-size: 3rem;
+                font-weight: 700;
+                line-height: 1.2;
+                color: var(--white);
+            }
+        }
+    }
 </style>
